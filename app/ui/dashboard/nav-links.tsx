a2e938eb6c8de4700307945fd8cfc6b6928cd881
Ignacio Fleming <ignacioflemings@gmail.com>
2025-07-25 17:08:35 -0300

    .

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,9 @@
-'use client';
+"use client";
 
 import clsx from "clsx";
 import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 
 const links = [
   { name: "Home", href: "/dashboard", icon: HomeIcon },
@@ -27,7 +26,7 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3", {
-              "bg-sky-100 text-blue-600": link.href===pathname,
+              "bg-sky-100 text-blue-600": link.href === pathname,
             })}
           >
             <LinkIcon className="w-6" />
